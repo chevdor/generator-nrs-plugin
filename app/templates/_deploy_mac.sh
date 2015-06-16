8d28bf59e00e329e4f3a22a3c5f2a848259e0b7c
Wilfried Kopp <chevdor@gmail.com>
2015-06-17 00:37:31 +0200

    Fix deploy script for Mac

--- app/templates/_deploy_mac.sh
+++ app/templates/_deploy_mac.sh
@@ -1,7 +1,7 @@
-#!/bin/bash 
+#!/bin/bash
 
 TARGET="/Applications/Nxt Wallet.app/Contents/Resources/nxt/html/ui/plugins/<%= pluginName %>/"
-
+mkdir "$TARGET"
 cp -Rfv ./css "$TARGET"
 cp -Rfv ./html "$TARGET"
 cp -Rfv ./js 	"$TARGET"
